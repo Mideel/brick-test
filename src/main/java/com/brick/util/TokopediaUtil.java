58907ffe573450a7af7b7ade03f5ef44b4905109
Mideel <mideel_neutral@yahoo.com>
2021-12-05 19:55:21 +0700

    Set headless browser

--- src/main/java/com/brick/util/TokopediaUtil.java
+++ src/main/java/com/brick/util/TokopediaUtil.java
@@ -43,7 +43,7 @@ public class TokopediaUtil {
         options.setHeadless(true);
         options.addArguments("user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36");
         webDriver = new ChromeDriver(options);
-        webDriver = new ChromeDriver();
+//        webDriver = new ChromeDriver();
     }
 
     public static void produceTopProductsInfoFile() throws Exception {
